03dc12feccd43fa9caceea3a8dfa2a46c3d3d4c4
GianniCrux <gianni.disti@gmail.com>
2024-08-16 12:22:15 +0200

    changed styles of project section

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,17 +1,30 @@
 import PropTypes from 'prop-types';
 
-const Project = ({ title, description, link, previewImage }) => {
+const Project = ({ title, description, imageUrl, link, previewImage }) => {
   return (
-    <div className="p-4">
-      <a href={link} target="_blank" rel="noopener noreferrer" className="block bg-black text-white shadow-lg rounded-lg overflow-hidden">
-        <div className="relative">
-          <img src={previewImage} alt={`${title} preview`} className="w-full h-48 object-cover" />
-        </div>
-        <div className="p-4">
-          <h3 className="text-lg font-bold mb-2">{title}</h3>
-          <p className="text-white">{description}</p>
+    <div className="relative group overflow-hidden border border-blue-400 rounded-lg shadow-md w-full max-w-xs">
+      <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4">
+        <h3 className="text-lg font-bold mb-2">{title}</h3>
+        <p className="text-white mb-4">{description}</p>
+        <div className="relative w-full aspect-video">
+          <img 
+            src={previewImage} 
+            alt={`${title} logo`} 
+            className="object-contain w-full h-full" 
+          />
         </div>
       </a>
+      <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4 w-full h-full items-center justify-center">
+          <div className="relative w-full h-full ">           
+            <img 
+              src={imageUrl} 
+              alt={`${title} screenshot`} 
+              className="object-contain w-full h-full" 
+            />
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
@@ -19,6 +32,7 @@ const Project = ({ title, description, link, previewImage }) => {
 Project.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired, // Required for hover effect
   link: PropTypes.string.isRequired,
   previewImage: PropTypes.string.isRequired,
 };
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,46 +1,54 @@
-import Project from "./project";
+import Project from './project';
 
 const projects = [
   {
     title: 'Sticks',
     description: 'Miro clone: offer the possibility to create plans and workflows and collaborate real time with other users within the same organization',
     previewImage: '/logo-sticks.svg',
+    imageUrl: '/sticks-screen.png', 
     link: 'https://sticks-gamma.vercel.app/',
-    /* previewImage: '/sticks-screen.png' */
+  },
+  {
+    title: 'Floating Lemons',
+    description: 'Simple landing page created to practice with 3D object and learn about three.js and animation.',
+    previewImage: '/lemons.png',
+    imageUrl: '/lemons-screen.png', 
+    link: 'https://floatinglemons.vercel.app/',
   },
   {
     title: 'GPlanner',
     description: 'Plan scheduling app',
     previewImage: '/moonbg.jpg',
+    imageUrl: '/planner-screen.png',
     link: 'https://project2.com',
-    /* previewImage: '/moonbg.jpg' */
   },
   {
     title: 'Highwaytodev',
     description: 'Cars dodging mini-game',
     previewImage: '/moonbg.jpg',
+    imageUrl: '/highway-screen.png', 
     link: 'https://project2.com',
-    /* previewImage: '/moonbg.jpg' */
   },
 ];
 
 export default function Projects() {
   return (
-    <section id="projects" className="min-h-screen bg-white text-black dark:bg-black dark:text-white py-16">
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Projects</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {projects.map((project, index) => (
-          <Project
-            key={index}
-            title={project.title}
-            description={project.description}
-            link={project.link}
-            previewImage={project.previewImage}
-          />
-        ))}
+    <section id="projects" className="min-h-screen bg-black text-white py-16">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold text-center mb-8 text-blue-600">Projects</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
+          {projects.map((project, index) => (
+            <Project
+              key={index}
+              title={project.title}
+              description={project.description}
+              imageUrl={project.imageUrl} 
+              link={project.link}
+              previewImage={project.previewImage} 
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </section>
   );
 }
