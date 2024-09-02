3263bae71050aadcb67fcec492a387f4b1e1d567
GianniCrux <gianni.disti@gmail.com>
2024-09-02 19:16:53 +0200

    changed carousel speed

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -67,7 +67,7 @@ export default function AboutMe() {
         if (carousel.scrollLeft >= carousel.scrollWidth - carousel.clientWidth) {
           carousel.scrollLeft = 0;
         }
-      }, 10);
+      }, 15); 
     };
 
     startAutoScroll();
