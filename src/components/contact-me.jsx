e80e90db496f86ed8155ccd2bd7fbe5d2e2814e6
GianniCrux <gianni.disti@gmail.com>
2024-09-02 19:18:59 +0200

    fixed hover on socials

--- src/components/contact-me.jsx
+++ src/components/contact-me.jsx
@@ -72,7 +72,7 @@ const ContactMe = () => {
                 </svg>
                 <span className="hidden sm:inline">Email</span>
               </a>
-              <a href="https://www.linkedin.com/in/giovanni-di-stefano-0b00bb202/" target='_blank' rel="noopener noreferrer" className='flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-amber-700 transition-colors'>
+              <a href="https://www.linkedin.com/in/giovanni-di-stefano-0b00bb202/" target='_blank' rel="noopener noreferrer" className='flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-900 transition-colors'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                   <rect x="2" y="9" width="4" height="12"></rect>
