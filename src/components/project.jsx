c352bdbac592ecaa230ce694b940802476101b79
GianniCrux <gianni.disti@gmail.com>
2024-09-16 13:31:58 +0200

    refactor(project): changed layout

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,9 +1,10 @@
+import { ArrowRightIcon } from 'lucide-react';
 import PropTypes from 'prop-types';
 
 const Project = ({ title, description, imageUrl, link, previewImage, passkey, width }) => {
   return (
     <div className="relative bg-white group overflow-hidden border border-amber-400 rounded-lg shadow-md w-full h-[250px] transform transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:h-[300px] hover:w-[105%] hover:translate-y-[-10px] hover:z-10">
-      <a href={link} target="_blank" rel="noopener noreferrer" className="block h-full">
+      <div className="block h-full">
         <div className="relative flex justify-center items-center w-full h-full">
           <img 
             src={previewImage} 
@@ -11,24 +12,50 @@ const Project = ({ title, description, imageUrl, link, previewImage, passkey, wi
             className={`object-cover ${width} h-full transition-opacity duration-300 group-hover:opacity-0`} 
           />
         </div>
-      </a>
+      </div>
       <div className="absolute inset-0 bg-black bg-opacity-90 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        <a href={link} target="_blank" rel="noopener noreferrer" className="block p-4 w-full h-full">
+        <div className="p-4 w-full h-full">
           <div className="flex flex-col justify-between h-full">
             <h3 className="text-2xl font-light mb-2 text-amber-600">{title}</h3>
             <div className="overflow-y-auto scrollbar-hide mb-4 max-h-[60%]">
               <p className="text-white text-sm">{description}</p>
-              {passkey && <pre className="text-xs text-gray-400 mt-2">{passkey}</pre>}
+              {passkey && (
+                <div className="flex justify-between items-center mt-2">
+                  <pre className="text-xs text-gray-400">{passkey}</pre>
+                  <a 
+                    href={link} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="border-none text-amber-400 px-3 py-1 rounded text-xs hover:text-yellow-200 transition-colors duration-300 flex items-center space-x-1 whitespace-nowrap"
+                  >
+                    <span className="underline">Discover More</span>
+                    <ArrowRightIcon size={12} />
+                  </a>
+                </div>
+              )}
+              {!passkey && (
+                <div className="flex justify-end mt-2">
+                  <a 
+                    href={link} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="border-none text-amber-400 px-3 py-1 rounded text-xs hover:text-yellow-200 transition-colors duration-300 flex items-center space-x-1 whitespace-nowrap"
+                  >
+                    <span className="underline">Discover More</span>
+                    <ArrowRightIcon size={12} />
+                  </a>
+                </div>
+              )}
             </div>
-            <div className="relative w-full h-24">
+            <div className="relative w-full h-24 flex items-end justify-between">
               <img 
                 src={imageUrl} 
                 alt={`${title} screenshot`} 
-                className="object-contain w-full h-full" 
+                className="object-contain w-3/4 h-full" 
               />
             </div>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
