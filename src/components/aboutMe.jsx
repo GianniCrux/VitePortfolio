37611bbfdf2e90ad974910275fa343cd0a588ac4
GianniCrux <gianni.disti@gmail.com>
2024-09-24 15:43:42 +0200

    restyle(aboutMe): changed description font and colors

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -7,7 +7,7 @@ import { useRef, useEffect, useState } from 'react';
 const TechIcon = ({ Icon, name }) => (
   <div className="flex flex-col items-center p-2 w-20 flex-shrink-0">
     <Icon className="text-xl md:text-2xl lg:text-3xl mb-1 text-amber-500" />
-    <span className="text-xs md:text-sm text-white font-lora">{name}</span>
+    <span className="text-xs md:text-sm text-black font-sans">{name}</span>
   </div>
 );
 
@@ -86,24 +86,24 @@ export default function AboutMe() {
 
 
   return (
-    <section id="about" className="py-10 bg-black text-black min-h-screen flex flex-col items-center justify-center">
+    <section id="about" className="py-10 bg-amber-300 text-black min-h-screen flex flex-col items-center justify-center">
       <div className="container mx-auto px-4 relative max-w-3xl">
         <div className="text-center mb-12">
-          <h2 className="text-3xl md:text-4xl text-amber-600 tracking-tighter font-playfair mb-6">About me</h2>
+          <h2 className="text-3xl md:text-4xl text-black tracking-tighter font-playfair mb-6">About me</h2>
           <div>
-            <p className="tracking-tighter text-white mb-4 font-lora text-sm md:text-base">
+            <p className="tracking-tighter text-black mb-4 font-sans text-sm md:text-base">
               As a dedicated web developer, I specialize in crafting user-centric, visually appealing websites with a strong focus on efficiency and scalability. Leveraging my expertise in React, Next.js, and modern web technologies, I build applications that not only meet high performance standards but also deliver a seamless user experience.
             </p>
-            <p className="tracking-tighter text-white font-lora text-sm md:text-base">
+            <p className="tracking-tighter text-black font-sans text-sm md:text-base">
             Beyond coding, I&apos;m an avid gamer and film enthusiast who thrives on adventures and challenges. My love for trekking and animals keeps me connected to nature, and I always aim to foster an inclusive environment where everyone feels at home. I&apos;m passionate about continuously evolving as a developer, embracing new technologies, and creating solutions that make a meaningful impact.
             </p>
           </div>
         </div>
         
         <div className="mt-16">
-          <h3 className="text-2xl md:text-3xl text-amber-500 tracking-tighter text-center font-playfair mb-8">Technologies i work with</h3>
+          <h3 className="text-2xl md:text-3xl text-black tracking-tighter text-center font-playfair mb-8">Technologies i work with</h3>
           <div 
-            className="overflow-hidden bg-gray-900 bg-opacity-50 rounded-lg p-4"
+            className="overflow-hidden bg-yellow-100 bg-opacity-50 rounded-lg p-4"
             onMouseDown={startDragging}
             onMouseUp={stopDragging}
             onMouseLeave={stopDragging}
