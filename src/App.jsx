7505b469144e6fdbcffad60f2c8896c4f98f47c7
GianniCrux <gianni.disti@gmail.com>
2024-10-15 16:38:44 +0200

    fixed(calendly): removed book an appointment section and embedded calendly inside contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import { Suspense, useEffect, useState } from 'react'
+import { Suspense} from 'react'
 import HomePage from './Home'
 import AboutMe from './components/aboutMe'
 import Projects from './components/projects'
 import ContactMe from './components/contact-me'
-import { Calendly } from './_components/calendly'
 import Navigation from './components/hamburger-menu'
 
 
 export default function App() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth < 568);
-    };
-
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-
-    return () => {
-      window.removeEventListener('resize', checkMobile);
-    };
-  }, []);
 
   return (
     <>
@@ -31,9 +16,6 @@ export default function App() {
         </Suspense>
       <AboutMe />
       <Projects />
-      {!isMobile && (
-      <Calendly />
-      )}
       <ContactMe />
     </>
   );
